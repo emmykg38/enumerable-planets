0e62aaf5e97986ea48d7c247f1a921e7bb9e0ffa
millylane003 <emilykgerst19@gmail.com>
2021-09-14 12:53:15 -0500

    exercise complete

--- src/main/java/org/launchcode/enumerableplanets/data/Planets.java
+++ src/main/java/org/launchcode/enumerableplanets/data/Planets.java
@@ -1,20 +1,21 @@
 package org.launchcode.enumerableplanets.data;
 
 public enum Planets {
-    MERCURY("Mercury", 88),
-    VENUS("Venus", 225),
-    EARTH("Earth", 365),
-    MARS("Mars", 687),
-    JUPITER("Jupiter", 4333),
-    SATURN("Saturn", 10759),
-    URANUS("Uranus", 30687),
-    NEPTUNE("Neptune", 60200);
+    MERCURY(1, "Mercury", 88),
+    VENUS(2, "Venus", 225),
+    EARTH(3, "Earth", 365),
+    MARS(4, "Mars", 687),
+    JUPITER(5, "Jupiter", 4333),
+    SATURN(6, "Saturn", 10759),
+    URANUS(7, "Uranus", 30687),
+    NEPTUNE(8,"Neptune", 60200);
 
-
+    private int number;
     private String name;
     private int yearLength;
 
-    Planets(String name, int yearLength) {
+    Planets(int number, String name, int yearLength) {
+        this.number = number;
         this.name = name;
         this.yearLength = yearLength;
     }
@@ -25,5 +26,9 @@ public enum Planets {
 
     public int getYearLength() {
         return yearLength;
+    }
+
+    public int getNumber() {
+        return number;
     }
 }
